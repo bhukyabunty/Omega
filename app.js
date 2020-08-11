05d291a76696cb5a7d7d46e10a4102a8382e3953
bhukyabunty <49990576+bhukyabunty@users.noreply.github.com>
2020-08-11 19:56:14 +0530

    Update app.js

--- app.js
+++ app.js
@@ -3,20 +3,22 @@
 
 ///Edit Under This only
 
-var firebaseConfig = {
-  apiKey: "api-key",
-  authDomain: "project-id.firebaseapp.com",
-  databaseURL: "https://project-id.firebaseio.com",
-  projectId: "project-id",
-  storageBucket: "project-id.appspot.com",
-  messagingSenderId: "sender-id",
-  appId: "app-id",
-  measurementId: "G-measurement-id",
+const firebaseConfig = {
+
+  apiKey: "AIzaSyCExmK4P5meRZboUARG_Pv0-PEY3Dq91A4",
+
+  authDomain: "dreaming-13351.firebaseapp.com",
+
+  databaseURL: "https://dreaming-13351.firebaseio.com",
+
+  projectId: "dreaming-13351",
+
+  storageBucket: "dreaming-13351.appspot.com",
+
+  messagingSenderId: "335796894364",
+
+  appId: "1:335796894364:web:6058e39eb6d80118a88b59",
+
+  measurementId: "G-32LEXCQKE1"
+
 };
-
-     
- 
-////Do Not Edit Below This
-
-firebase.initializeApp(firebaseConfig);
-var database = firebase.database();
